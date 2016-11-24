a1b3e6b744d0a05ae4f59b7ee5329cbfe4a40255
Rob Attfield <emailme@robertattfield.com>
2016-11-24 13:45:11 +1300

    Added functionality to convert wind speed on clicking unit measurement, refactored code.

--- src/openWeather.js
+++ src/openWeather.js
@@ -34,6 +34,10 @@
             customIcons: null,
             units: 'metric',
             windSpeedUnit: 'Kph',
+            windDirectionUnit: 'compass',
+            //clickConvertTemperature: true,
+            //clickConvertWindDirection: true,
+            clickConvertWindSpeed: true,
             city: null,
             lat: null,
             lng: null,
@@ -135,7 +139,6 @@
                 var temperatureUnit;
                 var minTemperature;
                 var maxTemperature;
-                var speedMeasurementUnit = "Mph";
 
                 switch(s.units){
                     case 'imperial':
@@ -145,7 +148,6 @@
                     case 'metric':
                         //Metric measurements use Centigrade/Celsius for temperature.
                         temperatureUnit = '°C';
-                        speedMeasurementUnit = "Kph";
                         break;
                     case 'standard':
                         //The standard temperature from the API uses Kelvin by default.
@@ -269,26 +271,70 @@
                     //Default speed from API is in meters per second (mps).
                     switch(s.windSpeedUnit){
                         case 'mps':
-                            windSpeed = Math.round(data.wind.speed) + ' ' + s.windSpeedUnit;
+                            windSpeed = (data.wind.speed);
                             break;
                         case 'Mph':
-                            windSpeed = Math.round(data.wind.speed / 0.44704) + ' ' + s.windSpeedUnit;
+                            windSpeed = (data.wind.speed / 0.44704);
                             break;
                         case 'Kph':
                             //No break termination, as this is the default.
-                            windSpeed = Math.round(data.wind.speed * 3.6) + ' ' + s.windSpeedUnit;
+                            windSpeed = (data.wind.speed * 3.6);
                     }
 
 
                     // set wind speed
-                    $(s.windSpeedTarget).text(windSpeed);
+                    $(s.windSpeedTarget).text(windSpeed.toFixed(1) + ' ' + s.windSpeedUnit);
+
+                    if(s.clickConvertWindSpeed == true){
+
+                        var convertWindSpeedClickCount = 0;
+
+                        //Using slice to remove units from value.
+                        var currentWindSpeedValue = $(s.windSpeedTarget).text().substring(0, 2);
+                        var windSpeedUnits = ['Kph','Mph','mps'];
+
+                        $(s.windSpeedTarget).on('click', function(e){
+                            convertWindSpeedClickCount++;
+                            var newWindSpeedUnit = windSpeedUnits[convertWindSpeedClickCount % windSpeedUnits.length];
+                            var newSpeed;
+
+
+                            newSpeed = convertWindSpeed(s.windSpeedUnit, newWindSpeedUnit, currentWindSpeedValue);
+                            $(s.windSpeedTarget).text(newSpeed);
+
+                        });
+                    }
                 }
 
                 // if windDirection isn't null
                 if(s.windDirectionTarget != null){
 
                     // set wind direction
-                    $(s.windDirectionTarget).text(data.wind.deg);
+                    var windDirection = data.wind.deg;
+
+                    if(s.windDirectionUnit === 'degrees') {
+                        $(s.windDirectionTarget).text(windDirection.toFixed(1) + '°');
+                    }
+                    if(s.windDirectionUnit === 'compass'){
+
+                        // The 16 general compass directions
+                        var compassDirections = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+
+                        // There are 16 general compass directions.
+                        // Dividing 360 by 16 equals 22.5.
+                        // 0.5 is added to prevent ties.
+                        var directionIndex = null;
+                        if($.isNumeric(windDirection))
+                        {
+                            if(windDirection >=0){
+                                directionIndex=+Math.round(((windDirection/22.5)+0.5)%16);
+                                $(s.windDirectionTarget).text(compassDirections[directionIndex]);
+                            }
+                        }
+                    }
+                    else{
+                        $(s.windDirectionTarget).text(windDirection.toFixed(1) + '°');
+                    }
                 }
 
                 // if humidityTarget isn't null
@@ -326,5 +372,43 @@
                 s.error.call(this, textStatus);
             }
         });
+
+
+
+        function convertWindSpeed(unitFrom, unitTo, value) {
+
+            if(value == null){
+                return null;
+            }
+            else {
+                var windSpeed;
+
+                if (unitFrom === 'Kph' && unitTo === 'Mph') {
+                    windSpeed = (value / 1.609344);
+                }
+                else if (unitFrom === 'Mph' && unitTo === 'Kph') {
+                    windSpeed = (value * 1.609344);
+                }
+                else if (unitFrom === 'Kph' && unitTo === 'mps') {
+                    windSpeed = (value / 3.6);
+                }
+                else if (unitFrom === 'Mph' && unitTo === 'mps') {
+                    windSpeed = (value * 0.44704);
+                }
+                else if (unitFrom === 'mps' && unitTo === 'Kph') {
+                    windSpeed = (value * 3.6);
+                }
+                else if (unitFrom === 'mps' && unitTo === 'Mph') {
+                    windSpeed = (value * 2.23693629);
+                }
+                else {
+                    return value + ' ' + unitFrom;
+                }
+
+                return windSpeed.toFixed(1)  + ' ' + unitTo;
+            }
+        }
     }
+
+
 })(jQuery);
